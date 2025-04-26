3bbc73a5e8d72e8c888496a6cb78dd087417f574
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2025-04-26 13:03:14 +0200

    Add files via upload

---
+++ KutyaClone/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace KutyaClone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "Hello";
+            s = s.ToUpper();
+            Console.WriteLine(s);
+        }
+    }
+
+    class Kutya: ICloneable
+    {
+        String név; // immutable
+        int lábakSzáma;
+        String[] bolhaNevek;
+        bool harapos;
+        public Object Clone()
+        {
+            Kutya myClone = new Kutya();
+            myClone.név = this.név;
+            myClone.lábakSzáma = this.lábakSzáma;
+            myClone.bolhaNevek = (String[])this.bolhaNevek.Clone();
+            myClone.harapos = this.harapos;
+            return myClone;
+        }
+    }
+}
